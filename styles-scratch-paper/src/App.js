87f233ee90511a46642a7a77bb3172c7815e8207
Robby Lewis <Robbysim37@gmail.com>
2023-08-23 17:03:13 -0400

    scratch paper

---
+++ styles-scratch-paper/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header></header>
+      <menu></menu>
+      <div className="hero"></div>
+      <main></main>
+      <div className="banner"></div>
+    </div>
+  );
+}
+
+export default App;
