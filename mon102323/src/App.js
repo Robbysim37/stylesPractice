1f182ef25661f83147d61b45762fcd6d30b6847f
Robby Lewis <Robbysim37@gmail.com>
2023-10-23 14:02:32 -0400

    monday 10/23/23

---
+++ mon102323/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className="grid">
+        <div className="item red"></div>
+        <div className="item orange"></div>
+        <div className="item yellow"></div>
+        <div className="item green"></div>
+        <div className="item lightGreen"></div>
+        <div className="item aqua"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
