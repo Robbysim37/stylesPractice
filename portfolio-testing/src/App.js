62d5dab609702c52d5fd0b5f379c9b0a9ec732b8
Robby Lewis <Robbysim37@gmail.com>
2023-10-27 19:46:59 -0400

    yay

--- portfolio-testing/src/App.js
+++ portfolio-testing/src/App.js
@@ -2,6 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Hexagon from './shapes/Hexagon';
 
+
+// Width as a base (100px)
+// Height is 110% of the base (110px)
+// surrounding margin is 5% of base (5px)
+// 32:50 ratio - 50 == half of width (50px)
+// 32:50 ratio - 32 == 64% of half of the width (32px)
+// margin left on every even row == same as surrounding margin (5px)
+
+
 function App() {
   return (
     <div className="App">
@@ -10,34 +19,16 @@ function App() {
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
+        <Hexagon name="clear"></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon name="clear"></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon name="clear"></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-      </div>
-      <div className='row'>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
+        <Hexagon name="clear"></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
       </div>
@@ -47,14 +38,32 @@ function App() {
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
+        <Hexagon name="clear"></Hexagon>
+        <Hexagon name="clear"></Hexagon>
+        <Hexagon name="clear"></Hexagon>
+        <Hexagon name="clear"></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon name="clear"></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon></Hexagon>
+      </div>
+      <div className='row'>
+        <Hexagon></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon></Hexagon>
+        <Hexagon name="clear"></Hexagon>
+        <Hexagon name="clear"></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
-        <Hexagon></Hexagon>
+        <Hexagon name="clear"></Hexagon>
+        <Hexagon name="clear"></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
         <Hexagon></Hexagon>
